c51602c58933426b28b4063fc9eeb766ee5e0893
hibiscus <yuy0815@icloud.com>
2025-12-28 15:37:56 +0900

    모델 로드

---
+++ WE/Sources/Render/SkeletalMesh.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "Utility/Class.h"
+#include "SkinnedData.h"
+
+#include <memory>
+
+struct SkinnedVertex
+{
+    DirectX::XMFLOAT3 Pos;
+    DirectX::XMFLOAT3 Normal;
+    DirectX::XMFLOAT2 TexC;
+    DirectX::XMFLOAT3 TangentU;
+    DirectX::XMFLOAT3 BoneWeights;
+    BYTE BoneIndices[4];
+};
+
+struct SkinnedModelInstance
+{
+    SkinnedData* SkinnedInfo = nullptr;
+    std::vector<DirectX::XMFLOAT4X4> FinalTransforms;
+    std::string ClipName;
+    float TimePos = 0.0f;
+
+    // Called every frame and increments the time position, interpolates the 
+    // animations for each bone based on the current animation clip, and 
+    // generates the final transforms which are ultimately set to the effect
+    // for processing in the vertex shader.
+    void UpdateSkinnedAnimation(float dt)
+    {
+        TimePos += dt;
+
+        // Loop animation
+        if (TimePos > SkinnedInfo->GetClipEndTime(ClipName))
+            TimePos = 0.0f;
+
+        // Compute the final transforms for this time position.
+        SkinnedInfo->GetFinalTransforms(ClipName, TimePos, FinalTransforms);
+    }
+};
+
+class FSkeletalMeshManager
+{
+	SINGLETON(FSkeletalMeshManager);
+public:
+	std::unique_ptr<SkinnedModelInstance> mSkinnedModelInst;
+};
