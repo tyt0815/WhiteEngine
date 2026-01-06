5db371d0749408c2961588d5342b6a8a38d07f90
hibiscus <yuy0815@icloud.com>
2026-01-06 18:59:21 +0900

    Update GameCore.h

--- WE/Sources/GameFramework/GameCore.h
+++ WE/Sources/GameFramework/GameCore.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <windows.h>
 
-class IGameApp
+namespace GameCore
 {
-public:
-	IGameApp() = default;
-	virtual ~IGameApp() = default;
+	class IGameApp
+	{
+	public:
+		IGameApp() = default;
+		virtual ~IGameApp() = default;
 
-	virtual void StartUp() = 0;
+		virtual void StartUp() = 0;
 
-	virtual void Update() = 0;
-};
+		virtual void Update() = 0;
+	};
+
+	int RunApplication
+}
+
+
 
 #define CREATE_APPLICATION(AppClass)\
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)\
