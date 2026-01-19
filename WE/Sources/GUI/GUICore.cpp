785f7febe1aa10991dd2ee062344b7f48ec0981a
hibiscus <yuy0815@icloud.com>
2026-01-19 18:47:37 +0900

    GUI DrawQueue 구현

--- WE/Sources/GUI/GUICore.cpp
+++ WE/Sources/GUI/GUICore.cpp
@@ -3,9 +3,19 @@
 #include "imgui_impl_dx12.h"
 #include "imgui_impl_win32.h"
 #include "DirectX/CBVSRVUAVHeap.h"
+#include <vector>
 
 namespace GUI
 {
+	std::vector<FDrawCommand> g_DrawCommandQueue;
+
+	UINT64 AddDrawCommand(const FDrawCommand& Command)
+	{
+		g_DrawCommandQueue.emplace_back(Command);
+		g_DrawCommandQueue.back().ID = g_DrawCommandQueue.size() - 1;
+		return g_DrawCommandQueue.back().ID;
+	}
+
 	void Initialize(
 		HWND hWnd,
 		ID3D12Device* Device, ID3D12CommandQueue* CommandQueue, DXGI_FORMAT RTVFormat
@@ -31,6 +41,41 @@ namespace GUI
 		ImGui_ImplDX12_Init(&InitInfo);
 		ImGui_ImplWin32_Init(hWnd);
 
+
+		//////////////////////////////////
+		// TestCode
+		FDrawCommand Command;
+		Command.LifeSpan = 5;
+		Command.DrawLambda = []()
+		{
+			ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+
+			ImGuiWindowFlags WindowFlags = ImGuiWindowFlags_NoDecoration |
+				ImGuiWindowFlags_AlwaysAutoResize |
+				ImGuiWindowFlags_NoSavedSettings |
+				ImGuiWindowFlags_NoFocusOnAppearing |
+				ImGuiWindowFlags_NoNav |
+				ImGuiWindowFlags_NoMove;
+
+			ImGui::SetNextWindowBgAlpha(1.0f);
+
+			if (ImGui::Begin("DebugOverlay", nullptr, WindowFlags))
+			{
+				ImGui::TextColored(ImVec4(1, 1, 0, 1), "WhiteEngine Debug"); // 노란색 제목
+				ImGui::Separator();
+
+				// FPS 정보 (ImGui 기본 제공)
+				ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+
+				// 우리가 보려고 하는 애니메이션 프레임 값
+				ImGui::Text("Current Frame: %.2f", 1);
+			}
+			ImGui::End();
+		};
+		AddDrawCommand(Command);		
+		// TestCode
+		//////////////////////////////////
+
 	}
 	void Update(ID3D12GraphicsCommandList* CommandList)
 	{
@@ -39,33 +84,22 @@ namespace GUI
 		ImGui_ImplWin32_NewFrame();
 		ImGui::NewFrame();
 
-		//////////////////////////////////
-		// TestCode
-		ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
-
-		ImGuiWindowFlags WindowFlags = ImGuiWindowFlags_NoDecoration |
-			ImGuiWindowFlags_AlwaysAutoResize |
-			ImGuiWindowFlags_NoSavedSettings |
-			ImGuiWindowFlags_NoFocusOnAppearing |
-			ImGuiWindowFlags_NoNav |
-			ImGuiWindowFlags_NoMove;
-
-		ImGui::SetNextWindowBgAlpha(1.0f);
-
-		if (ImGui::Begin("DebugOverlay", nullptr, WindowFlags))
+		for (int i = 0; i < g_DrawCommandQueue.size(); ++i)
 		{
-			ImGui::TextColored(ImVec4(1, 1, 0, 1), "WhiteEngine Debug"); // 노란색 제목
-			ImGui::Separator();
-
-			// FPS 정보 (ImGui 기본 제공)
-			ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
-
-			// 우리가 보려고 하는 애니메이션 프레임 값
-			ImGui::Text("Current Frame: %.2f", 1);
+			FDrawCommand& DrawCommand = g_DrawCommandQueue[i];
+			if (DrawCommand.LifeSpan < 0 || DrawCommand.DrawLambda == nullptr)
+			{
+				g_DrawCommandQueue[i] = g_DrawCommandQueue.back();
+				g_DrawCommandQueue[i].ID = i;
+				g_DrawCommandQueue.pop_back();
+				--i;
+			}
+			else
+			{
+				DrawCommand.DrawLambda();
+				g_DrawCommandQueue[i].LifeSpan -= ImGui::GetIO().DeltaTime;
+			}
 		}
-		ImGui::End();
-		// TestCode
-		//////////////////////////////////
 		
 
 		ImGui::Render();
--- WE/Sources/GUI/GUICore.h
+++ WE/Sources/GUI/GUICore.h
@@ -4,6 +4,15 @@
 
 namespace GUI
 {
+	struct FDrawCommand
+	{
+		UINT64 ID;
+		void (*DrawLambda)();
+		float LifeSpan;		// 0: 1프레임 그리기
+	};
+
+	UINT64 AddDrawCommand(const FDrawCommand& Command);
+
 	void Initialize(
 		HWND hWnd,
 		ID3D12Device* Device, ID3D12CommandQueue* CommandQueue, DXGI_FORMAT RTVFormat
